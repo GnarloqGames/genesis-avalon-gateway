3a1e4fdd8f9811c4b1704348c23ae8c057e7fcb8
Alfred Dobradi <alfreddobradi@gmail.com>
2024-02-19 15:24:17 +0000

    feat: Add release script, announce on Mastodon

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export GITHUB_TOKEN=$(vault kv get -mount=personal -format json goreleaser | jq .data.data.github_token)
+export MASTODON_CLIENT_ID=$(vault kv get -mount=personal -format json goreleaser | jq .data.data.mastodon_client_id)
+export MASTODON_CLIENT_SECRET=$(vault kv get -mount=personal -format json goreleaser | jq .data.data.mastodon_client_secret)
+export MASTODON_ACCESS_TOKEN=$(vault kv get -mount=personal -format json goreleaser | jq .data.data.mastodon_access_token)
+
+goreleaser release --clean

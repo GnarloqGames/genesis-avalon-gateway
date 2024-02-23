1d0689ff1aead751b0a3bc8616ef3b875b3e10a7
Alfred Dobradi <alfreddobradi@gmail.com>
2024-02-23 12:47:40 +0000

    feat: Add authorization middleware

--- platform/daemon/handler/handler.go
+++ platform/daemon/handler/handler.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/GnarloqGames/genesis-avalon-gateway/platform/auth"
 	"github.com/GnarloqGames/genesis-avalon-kit/proto"
 	"github.com/GnarloqGames/genesis-avalon-kit/transport"
 	"github.com/go-chi/chi/v5"
+	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -14,6 +16,7 @@ func Handler(bus *transport.Connection) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(LoggingMiddleware())
+	r.Use(auth.Middleware())
 
 	r.Post("/build", Build(bus))
 
@@ -22,6 +25,25 @@ func Handler(bus *transport.Connection) http.Handler {
 
 func Build(bus *transport.Connection) http.HandlerFunc {
 	fn := func(w http.ResponseWriter, r *http.Request) {
+		claims, ok := r.Context().Value(auth.ClaimsContext).(*auth.Claims)
+		if !ok || claims == nil {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		if !claims.HasRole("dev.avalon.cool:can-build") {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		context, err := structpb.NewStruct(map[string]any{
+			"owner": claims.Subject,
+		})
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
 		req := &proto.BuildRequest{
 			Header: &proto.RequestHeader{
 				TraceID:   "",
@@ -29,6 +51,7 @@ func Build(bus *transport.Connection) http.HandlerFunc {
 			},
 			Name:     "house",
 			Duration: "10s",
+			Context:  context,
 		}
 
 		var res proto.BuildResponse

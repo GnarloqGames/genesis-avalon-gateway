d22fd3e6d4d1ce4a28533772774d8ce15d8c4584
Alfred Dobradi <alfreddobradi@gmail.com>
2024-02-24 14:28:03 +0000

    ci: Fix release script

--- release.sh
+++ release.sh
@@ -2,19 +2,19 @@
 echo "Getting secrets from Vault..."
 secrets=$(vault kv get -mount=personal -format json goreleaser)
 echo -ne "\tGithub token..."
-GITHUB_TOKEN=$(echo $secrets | jq -r .data.data.github_token)
+export GITHUB_TOKEN=$(echo $secrets | jq -r .data.data.github_token)
 echo -ne " done\n"
 
 echo -ne "\tMastodon Client ID..."
-MASTODON_CLIENT_ID=$(echo $secrets | jq -r .data.data.mastodon_client_id)
+export MASTODON_CLIENT_ID=$(echo $secrets | jq -r .data.data.mastodon_client_id)
 echo -ne " done\n"
 
 echo -ne "\tMastodon Client Secret..."
-MASTODON_CLIENT_SECRET=$(echo $secrets | jq -r .data.data.mastodon_client_secret)
+export MASTODON_CLIENT_SECRET=$(echo $secrets | jq -r .data.data.mastodon_client_secret)
 echo -ne " done\n"
 
 echo -ne "\tMastodon Access Token..."
-MASTODON_ACCESS_TOKEN=$(echo $secrets | jq -r .data.data.mastodon_access_token)
+export MASTODON_ACCESS_TOKEN=$(echo $secrets | jq -r .data.data.mastodon_access_token)
 echo -ne " done\n"
 
 branch=$(git rev-parse --abbrev-ref HEAD)
